05ecbe3c1bc9ce5d2fd78f3db847b46915590bea
Daniel Granger <dg1234uk@me.com>
2019-03-24 02:18:30 +0000

    :lipstick: Updated alignment and size of sidebar copyright and social icons

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import getIcon from '../utils/get-icon';
 
-const Copyright = () => (
-  <div className="copyright">
-    <a
-      href="https://creativecommons.org/licenses/by/4.0/"
-      style={{ color: '#b6b6b6', textDecoration: 'none' }}
-    >
-      <svg
-        className="social__svg no-link-color"
-        viewBox={getIcon('creativecommons').viewBox}
+const Copyright = () => {
+  const iconSize = '1.2rem';
+  const marginBottom = '-0.30rem';
+  return (
+    <div className="copyright">
+      <a
+        href="https://creativecommons.org/licenses/by/4.0/"
+        style={{ color: '#b6b6b6', textDecoration: 'none' }}
       >
-        <path d={getIcon('creativecommons').path} />
-      </svg>
-      &nbsp;
-      <svg
-        className="social__svg"
-        viewBox={getIcon('creativecommonsby').viewBox}
-      >
-        <path d={getIcon('creativecommonsby').path} />
-      </svg>
-      <span> Creative Commons (CC BY 4.0).</span>
-    </a>
-  </div>
-);
+        <svg
+          className="social__svg"
+          style={{ height: iconSize, width: iconSize, marginBottom }}
+          viewBox={getIcon('creativecommons').viewBox}
+        >
+          <path d={getIcon('creativecommons').path} />
+        </svg>
+        &nbsp;
+        <svg
+          className="social__svg"
+          style={{ height: iconSize, width: iconSize, marginBottom }}
+          viewBox={getIcon('creativecommonsby').viewBox}
+        >
+          <path d={getIcon('creativecommonsby').path} />
+        </svg>
+        <span> Creative Commons (CC BY 4.0).</span>
+      </a>
+    </div>
+  );
+};
 
 export default Copyright;
